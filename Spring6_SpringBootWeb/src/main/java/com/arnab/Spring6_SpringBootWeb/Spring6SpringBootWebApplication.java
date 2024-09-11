0209c462685b5a0d80dd00eb484ed258978d0ecf
ARNAB BASAK <basakarnab912@gmail.com>
2024-09-11 12:39:50 +0530

    Added Spring6_SpringBootWeb

---
+++ Spring6_SpringBootWeb/src/main/java/com/arnab/Spring6_SpringBootWeb/Spring6SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.Spring6_SpringBootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ Spring6_SpringBootWeb/src/test/java/com/arnab/Spring6_SpringBootWeb/Spring6SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.Spring6_SpringBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
