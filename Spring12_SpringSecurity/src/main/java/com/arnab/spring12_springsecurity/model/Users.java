c068a1018b4b078a86a253aa280cc4f3c346f97d
ARNAB BASAK <basakarnab912@gmail.com>
2024-09-22 21:30:59 +0530

    Added Users

---
+++ Spring12_SpringSecurity/src/main/java/com/arnab/spring12_springsecurity/model/Users.java
@@ -0,0 +1,21 @@
+package com.arnab.spring12_springsecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    @Id
+    int id;
+    String username;
+    String password;
+}
