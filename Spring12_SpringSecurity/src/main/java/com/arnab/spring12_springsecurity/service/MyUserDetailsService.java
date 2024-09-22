2cc33c9d07d9093bbbaa57c0971fc5f19fc37b48
ARNAB BASAK <basakarnab912@gmail.com>
2024-09-22 21:25:45 +0530

    Added Repo and Service Layer

---
+++ Spring12_SpringSecurity/src/main/java/com/arnab/spring12_springsecurity/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.arnab.spring12_springsecurity.service;
+
+import com.arnab.spring12_springsecurity.model.Users;
+import com.arnab.spring12_springsecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user=repo.findByUsername(username);
+        if(user==null) {
+            System.out.println("User 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+        return new UserPrincipal(user);
+    }
+}
---
+++ Spring12_SpringSecurity/src/main/java/com/arnab/spring12_springsecurity/service/UserService.java
@@ -0,0 +1,20 @@
+package com.arnab.spring12_springsecurity.service;
+
+import com.arnab.spring12_springsecurity.model.Users;
+import com.arnab.spring12_springsecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo repo;
+//    creating BCryptEncoder
+    private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(10);
+    public void addUser(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
+        repo.save(user);
+    }
+}
