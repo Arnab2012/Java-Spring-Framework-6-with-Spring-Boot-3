9ec731f130296bd335b117454cc4b6a28af303e2
ARNAB BASAK <basakarnab912@gmail.com>
2024-09-25 21:58:53 +0530

    Added Spring14_OAuth2

---
+++ Spring14_OAuth2/src/main/java/com/arnab/Spring14_OAuth2/Spring14OAuth2Application.java
@@ -0,0 +1,13 @@
+package com.arnab.Spring14_OAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring14OAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring14OAuth2Application.class, args);
+	}
+
+}
