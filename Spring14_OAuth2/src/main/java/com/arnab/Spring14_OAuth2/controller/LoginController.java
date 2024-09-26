054070b3eda2424b3bb174854acf184a2acd8ced
ARNAB BASAK <basakarnab912@gmail.com>
2024-09-26 12:18:10 +0530

    Update LoginController.java

--- Spring14_OAuth2/src/main/java/com/arnab/Spring14_OAuth2/controller/LoginController.java
+++ Spring14_OAuth2/src/main/java/com/arnab/Spring14_OAuth2/controller/LoginController.java
@@ -1,4 +1,33 @@
 package com.arnab.Spring14_OAuth2.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
+
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String, String> loginData) {
+        String username = loginData.get("username");
+        String password = loginData.get("password");
+
+        System.out.println(username);
+        System.out.println(password);
+
+        // Dummy verification (Replace this with your user validation logic)
+        if ("Arnab".equals(username) && "ayan".equals(password)) {
+            return ResponseEntity.ok("Login successful!");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
 }
+
