4dfb8b1ce37f20227de6d186c2c2d5cfe2530495
ARNAB BASAK <basakarnab912@gmail.com>
2024-10-04 12:49:10 +0530

    Comments added

--- QuizApp/src/main/java/com/arnab/quizapp/controller/QuestionController.java
+++ QuizApp/src/main/java/com/arnab/quizapp/controller/QuestionController.java
@@ -9,22 +9,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("question")
+@RequestMapping("question") // common mapping for all request
 public class QuestionController {
 
     @Autowired
     QuestionService service;
 
+//  to get all questions from database
     @GetMapping("allquestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
         return service.getAllQuestions();
     }
 
+//  to get questions for a Particular category from database
     @GetMapping("getquestion/{category}")
     public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return service.getQuestionByCategory(category);
     }
 
+//  to add question in the database
     @PostMapping("addquestion")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return service.addQuestion(question);
--- QuizApp/src/main/java/com/arnab/quizapp/controller/QuizController.java
+++ QuizApp/src/main/java/com/arnab/quizapp/controller/QuizController.java
@@ -10,22 +10,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("quiz")
+@RequestMapping("quiz") // common mapping for all request
 public class QuizController {
 
     @Autowired
     QuizService service;
 
+//  to create a quiz
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ,@RequestParam String title){
         return service.createQuiz(category,numQ,title);
     }
 
+//  get the quiz questions
     @GetMapping("get/{title}")
     public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable String title){
         return service.getQuiz(title);
     }
 
+//  submit a quiz and get score
     @PostMapping("submit/{title}")
     public ResponseEntity<Integer> submitQuiz(@PathVariable String title, @RequestBody List<UserResponse> response){
         return service.submitQuiz(title,response);
